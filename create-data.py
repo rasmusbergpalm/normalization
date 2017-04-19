98526f4a181f214bd07cfd0a98a624c74a64760e
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2017-04-19 13:47:03 +0200

    more input formats

---
+++ create-data.py
@@ -0,0 +1,38 @@
+import datetime
+import random
+from typing import List
+
+import babel
+from babel.dates import format_date
+
+
+def writelines(lines: List[str], fname: str):
+    lines = [l + "\n" for l in lines]
+    with open(fname, "w") as f:
+        f.writelines(lines)
+
+
+formats = ["short", "medium", "long"]
+locales = babel.localedata.locale_identifiers()
+
+
+def create_data(prefix: str, N: int, create_vocab: bool):
+    startdate = datetime.date(1900, 1, 1)
+    target = []
+    source = []
+    for i in range(N):
+        date = startdate + datetime.timedelta(random.randint(0, 200 * 365))
+        target.append(date.isoformat())
+
+        datestr = format_date(date, format=random.choice(formats), locale=random.choice(locales))
+        source.append(datestr)
+
+    writelines(source, "%s-source.txt" % prefix)
+    writelines(target, "%s-target.txt" % prefix)
+    if create_vocab:
+        vocab = list(set("".join(target)).union(set("".join(source))))
+        writelines(vocab, "vocab.txt")
+
+
+create_data("train", 1000000, True)
+create_data("dev", 100000, False)
